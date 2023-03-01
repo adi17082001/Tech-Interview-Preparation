799c392a06dcd595edf15c26c413d3de2c17a84d
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2023-03-01 16:42:48 +0530

    Create Print_all_subsequences.cpp

---
+++ Recursion/Print_all_subsequences.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printF(int ind,vector<int>&ds, int arr[],int n){
+	if(ind==n){
+		for(auto it:ds){
+			cout<<it<<" ";
+		}
+		if(ds.size()==0){  // to print the empty subsequence
+			cout<<"[]";  
+		}
+		cout<<endl;
+		return;
+	}
+	// take or pick the particular index into the subsequence
+	ds.push_back(arr[ind]);
+	printF(ind+1,ds,arr,n);
+	ds.pop_back();
+
+	// not pick, or not take condition, this element is not added to your subsequence
+	printF(ind+1,ds,arr,n);
+}
+
+int main(){
+	int arr[] = {3,1,2};
+	int n= 3;
+	vector<int>ds;
+	printF(0,ds,arr,n);
+}
+
+// Time complexity = O(n(2^n)) approx
+// stack space = O(n)
