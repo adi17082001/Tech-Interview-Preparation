976e655abc5a3ae0c0ac35007bff415cdd798a25
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2023-06-15 13:16:31 +0530

    Create lowestHighestPrimes.cpp

---
+++ Numbers/lowestHighestPrimes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e7+10;
+
+vector<bool>isPrime(N,1);
+vector<int>lp(N,0), hp(N,0);
+
+int main(){
+	isPrime[0] = isPrime[1] = false;
+	for(int i=2;i<N;i++){
+		if(isPrime[i]==true){
+			lp[i] = hp[i] = i;
+			for(int j=2*i;j<N;j=j+i){   /// imp -> here we have taken j = j+i i.e. j is he multiple of i
+				isPrime[j] = false;
+				hp[j] = i;
+				if(lp[j]==0){
+					lp[j] = i;
+				}
+			}
+		}
+	}
+	for(int i=1;i<10;i++){
+		cout<<lp[i]<<" "<<hp[i]<<endl;
+	}
+}
