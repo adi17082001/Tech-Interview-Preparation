26dab52c814817e3c27a9bb2005fdb520055f44e
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2023-06-30 10:33:34 +0530

    Create happyNumber_IMPORTANT.cpp

---
+++ Numbers/happyNumber_IMPORTANT.cpp
@@ -0,0 +1,26 @@
+// Leetcode 202
+
+class Solution {
+ public:
+  bool isHappy(int n) {
+    int slow = squaredSum(n);
+    int fast = squaredSum(squaredSum(n));
+
+    while (slow != fast) {
+      slow = squaredSum(slow);
+      fast = squaredSum(squaredSum(fast));
+    }
+
+    return slow == 1;
+  }
+
+ private:
+  int squaredSum(int n) {
+    int sum = 0;
+    while (n) {
+      sum += pow(n % 10, 2);
+      n /= 10;
+    }
+    return sum;
+  };
+};
