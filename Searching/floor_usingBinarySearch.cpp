ef843f0211c57ae0db36d33a0d2cb000e9e88128
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2023-08-04 09:02:57 +0530

    Create floor_usingBinarySearch.cpp

---
+++ Searching/floor_usingBinarySearch.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ceilingInSortedArray(int n, int x, vector<int> &arr){  
+    int ans = -1;
+
+    // Initial range of search
+    int l = 0, h = n-1;
+    while(l <= h){
+
+        int mid = (l + h) / 2;
+
+        // Array element at Mid is greater than x.
+        if(arr[mid] >= x){
+            ans = arr[mid];
+
+            // We reduce search space to left half.
+            h = mid - 1;
+        }
+        else{
+
+            // We reduce search space to right half.
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int>arr = {10,20,30,40,50};
+    cout<<ceilingInSortedArray(5,25,arr);
+}
