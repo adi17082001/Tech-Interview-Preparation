9a2330d12cc003abcb9ae990fae2794b1dab906b
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2023-08-04 09:40:08 +0530

    Update ceil_usingBinarySearch.cpp

--- Searching/ceil_usingBinarySearch.cpp
+++ Searching/ceil_usingBinarySearch.cpp
@@ -1,30 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int ceilingInSortedArray(vector<int>&arr, int n, int x){  
-    int ans = -1;
-
-    // Initial range of search
-    int l = 0, h = n-1;
-    while(l <= h){
-
-        int mid = (l + h) / 2;
-
-        // Array element at Mid is greater than x.
-        if(arr[mid] >= x){
-            ans = arr[mid];
-
-            // We reduce search space to left half.
-            h = mid - 1;
-        }
-        else{
-
-            // We reduce search space to right half.
-            l = mid + 1;
-        }
-    }
-    return ans;
+long long int ceil(int arr[], int n, int x){
+	long long  ans = -1;
+	int low = 0, high = n-1;
+	while(low<=high){
+		int mid = (low+high)/2;
+		if(arr[mid]>=x){
+			ans = arr[mid];
+			high = mid -1 ;
+		}else{
+			low = mid + 1;
+		}
+	}
+	return ans;
 }
 int main(){
     vector<int>arr = {10,20,30,40,50};
-    cout<<ceilingInSortedArray(5,25,arr);
+    cout<<ceil(5,25,arr);
 }
