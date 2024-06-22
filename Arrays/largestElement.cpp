e73de66c2f9a9499642de51dff463ceedcd2a3cd
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2024-06-22 16:26:32 +0530

    Update largestElement.cpp

--- Arrays/largestElement.cpp
+++ Arrays/largestElement.cpp
@@ -2,40 +2,27 @@
 using namespace std;
 
 
-//Naive approach:
-int largestElement(int arr[], int n){
-	for(int i=0;i<n;i++){
-		bool flag = true;
+int largest(vector<int>&arr){
+	int n = arr.size();
+	// brute force
+	// sort the array and print arr[n-1];
+	// mergeSort(arr,0,n-1);
+	// return arr[n-1];
+	// it takes O(NlogN);
 
-
-		for(int j=0;j<n;j++){
-			if(arr[j]>arr[i]){
-				flag = false;
-				break;
-			}
-		}
-		if(flag==true){
-			return i;
-		}
-	}
-	return -1;
-}
-
-//Efficient approach.
-
-int getLargestElement(int arr[], int n){
-	int res = 0;
-	for(int i=1;i<n;i++){
-		if(arr[i]>arr[res]){
-			res = i;
+	// better
+	int largest = arr[0];
+	for(int i = 1; i < n; i++){
+		if(arr[i] > largest){
+			largest = arr[i] ;
 		}
 	}
-	return res;
+	return largest;
 }
 
 int main(){
-	int arr[] = {1,2,3,4,5};
-	cout<<getLargestElement(arr,5);
+	vector<int>arr = {1,2,3,4,5};
+	cout<<largest(arr);
 }
 
 
