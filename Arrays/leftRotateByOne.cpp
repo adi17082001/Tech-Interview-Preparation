b8c430d3142574939eb431a7eaec7100881ecc6c
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2024-06-25 17:40:05 +0530

    Update leftRotateByOne.cpp

--- Arrays/leftRotateByOne.cpp
+++ Arrays/leftRotateByOne.cpp
@@ -1,18 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void leftRotateByOne(int arr[], int n){
-  int temp = arr[0];
-  for(int i=1;i<n;i++){
-    arr[i-1] = arr[i];
-  }
-  arr[n-1] = temp;
+void leftRotateByOne(vector<int>&arr){
+	int n = arr.size();
+	int temp = arr[0];
+	for(int i = 1; i < n; i++){
+		arr[i-1] = arr[i];
+	}
+	arr[n-1] = temp;
 }
 
+
 int main(){
-  int arr[5] = {1,2,3,4,5};
-  leftRotateByOne(arr,5);
-  for(int i=0;i<5;i++){
-    cout<<arr[i]<<" ";
-  }
+	vector<int>arr = {1,2,3,4,56,7};
+	leftRotateByOne(arr);
+	for(auto i: arr){
+		cout<<i<<" ";
+	}
 }
