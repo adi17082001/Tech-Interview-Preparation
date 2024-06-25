8f304dba78b9de4dbfa89e1e966d7064c4319ad6
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2024-06-25 18:08:29 +0530

    Update leftRotateByD.cpp

--- Arrays/leftRotateByD.cpp
+++ Arrays/leftRotateByD.cpp
@@ -1,22 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-void reverseArray(int arr[], int n, int low, int high){
-    while(low<high){
-        swap(arr[low], arr[high]);
-        low++;
-        high--;
-    }
-}
-void leftRotate(int arr[], int n, int d){
-    reverseArray(arr,n,0,d-1);
-    reverseArray(arr,n,d,n-1);
-    reverseArray(arr,n,0,n-1);
+
+void reverse(int arr[], int low, int high){
+	while(low <= high){
+		swap(arr[low], arr[high]);
+		low++;
+		high--;
+	}
 }
 
+void leftRotateByD(int arr[], int n , int d){
+
+	/*brute
+
+	// copy elements from 0 to d-1 in temp
+	int temp[d];
+	for(int i = 0; i < d; i++){
+		temp[i] = arr[i];
+	}
+
+	// shifting elements from d to n-1
+	for(int i = d; i < n; i++){
+		arr[i-d] = arr[i];
+	}
+
+	// pushing back elements from temp to arr
+	for(int i = n-d; i < n; i++){
+		arr[i] = temp[i - (n-d)];
+	}
+
+	*/
+
+	/* Optimal */
+
+	reverse(arr, arr + d);
+	reverse(arr + d, arr + n);
+	reverse(arr, arr + n);
+}
+
+
 int main(){
-    int arr[5] = {1,2,3,4,5};
-    leftRotate(arr,5,2);
-    for(int i=0;i<5;i++){
-        cout<<arr[i]<<" ";
-    }
+	int arr[7] = {1,2,3,4,5,6,7};
+	leftRotateByD(arr,7,3);
+	for(int i = 0; i < 7; i++){
+		cout<<arr[i]<<" ";
+	}
 }
