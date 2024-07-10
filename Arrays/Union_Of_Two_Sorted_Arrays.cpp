df14011a108ae6930f8de18ce093cc4f69b9cfb1
adi17082001 <73772113+adi17082001@users.noreply.github.com>
2024-07-10 18:15:35 +0530

    Update Union_Of_Two_Sorted_Arrays.cpp

--- Arrays/Union_Of_Two_Sorted_Arrays.cpp
+++ Arrays/Union_Of_Two_Sorted_Arrays.cpp
@@ -1,6 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// brute approach
+vector<int>unionOfSortedArrays_brute(vector<int>&a, vector<int>&b){
+	int n1 = a.size();
+	int n2 = b.size();
+
+	// push all elements into the ordered set.
+	set<int>st;
+	for(int i = 0; i < n1; i++){
+		st.insert(a[i]);
+	}
+	for(int i = 0; i < n2; i++){
+		st.insert(b[i]);
+	}
+
+	// declare the union array
+	int x = st.size();
+	vector<int>unionArr(x);
+	int i = 0;
+	for(auto it: st){
+		unionArr[i] = it;
+		i++;
+	}
+
+	return unionArr;
+
+}
+
+// T.C. = O(n1logN) + O(n2logN) + O(n1 + n2)
+// S.C. = O(n1 + n2) + O(n1 + n2)
+
+
 
 // optimal approach 
 vector<int> unionOfSortedArrays(vector<int>&a, vector<int>&b){
@@ -49,7 +80,7 @@ vector<int> unionOfSortedArrays(vector<int>&a, vector<int>&b){
 int main(){
 	vector<int>a = {1,1,2,3,4,5};
 	vector<int>b = {2,3,3,4,5,6};
-	vector<int>unionArr = unionOfSortedArrays_brute(a,b);
+	vector<int>unionArr = unionOfSortedArrays(a,b);
 	for(auto i: unionArr){
 		cout<<i<<" ";
 	}
